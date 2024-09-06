bb3cf4aca839807a33b8cdb745fb07f87483178c
Vijay-dev01 <vijayajay3535@gmail.com>
2024-09-06 13:00:24 +0530

    multer add for the create project images.

--- Backend/controllers/projectscontroller.js
+++ Backend/controllers/projectscontroller.js
@@ -20,6 +20,20 @@ exports.getProjects = async (req, res, next) => {
 };
 
 exports.newProject = catchAsyncError(async (req, res, next) => {
+  let images = []
+  let BASE_URL = process.env.BACKEND_URL;
+  if(process.env.NODE_ENV === "production"){
+      BASE_URL = `${req.protocol}://${req.get('host')}`
+  }
+  
+  if(req.files.length > 0) {
+      req.files.forEach( file => {
+          let url = `${BASE_URL}/uploads/projects/${file.originalname}`;
+          images.push({ image: url })
+      })
+  }
+
+  req.body.images = images;
   req.body.user = req.user.id;
   const project = await Project.create(req.body);
   res.status(201).json({
--- Backend/routes/project.js
+++ Backend/routes/project.js
@@ -8,12 +8,25 @@ const {
 } = require("../controllers/projectscontroller");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/authenticate");
 const router = express.Router();
+const multer = require('multer');
+const path = require('path')
 
-router.route("/projects").get(isAuthenticatedUser, getProjects);
+const upload = multer({storage: multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join( __dirname,'..' , 'uploads/projects' ) )
+    },
+    filename: function(req, file, cb ) {
+        cb(null, file.originalname)
+    }
+}) })
+
+
+
+router.route("/projects").get(getProjects);
 router.route("/project/:id").get(getSingleProject)
 .put(updateProject)
 .delete(deleteProject)
 
 //Admin routes
-router.route("/admin/project/new").post(isAuthenticatedUser, authorizeRoles("admin"), newProject);
+router.route("/admin/project/new").post(isAuthenticatedUser, authorizeRoles('admin'), upload.array('images'), newProject);
 module.exports = router;
